0a978620a0fe0a03f77d431ef060688f78809a99
Teja0707 <44122877+Teja0707@users.noreply.github.com>
2021-01-05 11:57:05 -0600

    Microservices with spring

---
+++ S07L02-WebappWithSpringBoot/src/main/java/org/webdev/S07L02WebappWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.webdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S07L02WebappWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S07L02WebappWithSpringBootApplication.class, args);
+	}
+
+}
