b4f8e6d3e161f30074797b661608dfef306c10e7
Teja0707 <44122877+Teja0707@users.noreply.github.com>
2021-01-05 12:00:24 -0600

    Passing param with resource types : JAX RS Demo

---
+++ S05L04-ParamWithResourceType/src/main/java/org/webdev/App.java
@@ -0,0 +1,23 @@
+package org.webdev;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/app/{demo}")
+public class App {
+
+	@QueryParam("x") String x;
+	@PathParam("demo") String demo;
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String demo() {
+		return "The value of x is " + x + "||  Value of demo " + demo;
+	}
+}
---
+++ S05L04-ParamWithResourceType/src/main/java/org/webdev/AppConfig.java
@@ -0,0 +1,11 @@
+package org.webdev;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("JAXRS")
+public class AppConfig extends Application{
+	
+	
+
+}
